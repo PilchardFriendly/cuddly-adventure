2a5f3f5895844d2617599b8e921c54f41284efca
Nicholas Drew <code@nickdrew.io>
2020-05-10 00:40:13 +1000

    The accelerate spec passes! that was ... hard

--- test/Shokinin20/Accelerate/Spec.hs
+++ test/Shokinin20/Accelerate/Spec.hs
@@ -1,18 +1,24 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+
 {-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE ConstraintKinds     #-}
 {-# LANGUAGE FlexibleContexts    #-}
-{-# LANGUAGE PatternSynonyms #-}
+
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE GeneralisedNewtypeDeriving #-}
 {-# LANGUAGE StandaloneDeriving #-}
+
+{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE ViewPatterns          #-}
-{-# LANGUAGE TypeOperators         #-}
 
 module Shokinin20.Accelerate.Spec (spec) where
 import Prelude hiding (zipWith)
@@ -22,11 +28,10 @@ import Test.Hspec.Expectations
 import Data.Array.Accelerate as A
 import Data.Array.Accelerate.Smart as S
 import Data.Array.Accelerate (Stencil3x3, stencil, Exp(..))
--- import Data.Array.Accelerate.Pattern as Pattern
-import Data.Array.Accelerate.Type (TupleType(..))
--- import Data.Array.Accelerate.Product (ProdR(..), TupleIdx(..))
--- import Data.Array.Accelerate.Array.Sugar (Elt(..), TupleRepr(..))
+import Data.Array.Accelerate.Product ()
+-- import Data.Array.Accelerate.Pattern as AP
 import Data.Array.Accelerate.Data.Semigroup (Min(..))
+import Data.Array.Accelerate.Interpreter  as Interpreter
 import Data.Array.Accelerate.LLVM.Native  as CPU
 import Prelude as P
 
@@ -34,17 +39,51 @@ type Stencil3x3x3x3 a = (Stencil3x3x3 a, Stencil3x3x3 a, Stencil3x3x3 a)
 
 
 spec :: Spec
-spec = describe "dotp" do
-    it "should calculate dot product" do
-        CPU.run $ sample1x1x3x3    
+spec = describe "Stepping" do
+    describe "plus'"
+        let
+            step1 :: Acc (A.Vector  L) -> Acc (A.Vector L)
+            step1 = A.map (plus' lOne)
+        in do
+            it "one" 
+                let
+                    !go = Interpreter.runN (step1 $ A.use $ A.fromList (Z:.3) [L 0, L 1, L 2])
+                in do
+                    (toList $ go) `shouldBe` (L <$> [100, 2, 3])
+    describe "lmin"
+        let
+            step1 :: Acc (A.Vector L) -> Acc (A.Vector  L)-> Acc (A.Vector L)
+            step1 as bs = A.zipWith lmin as bs
+        in do
+            it "all" 
+                let
+                    as = A.use $ A.fromList (Z:.3) [L 0, L 1, L 100]
+                    bs = A.use $ A.fromList (Z:.3) [L 1, L 1, L 1]
+                    !go = Interpreter.runN (step1 as bs)
+                in do
+                    (toList $ go) `shouldBe` (L <$> [0, 1, 1])
+
+    describe "stepping" do
+        it "should calculate starting position" 
+            let
+                !go = Interpreter.runN sample1x1x3x3 
+            in do
+                (toList go) `shouldBe` (L <$> [0,100,100,100,1,100,100,100,0])
+        it "should calculate first step position" 
+            let
+                !go = Interpreter.runN (stepNPXY sample1x1x3x3)
+            in do
+                (toList go) `shouldBe` (L <$> [0,2,100,2,1,2,100,2,0])
 
 
 dotp :: Acc (Vector Float) -> Acc (Vector Float) -> Acc (Scalar Float)
 dotp xs ys = A.fold (+) 0 (A.zipWith (*) xs ys)
 
 newtype L = L Word8
-  deriving (Show, Generic, Elt)
-deriving instance (IsProduct Elt Word8) => IsProduct Elt L
+  deriving (Show, Generic, Elt, P.Eq) 
+
+instance IsProduct Elt L
+
 
 pattern Top :: S.Exp L
 pattern Top = S.Exp (S.Const (L 100))
@@ -52,33 +91,29 @@ pattern Top = S.Exp (S.Const (L 100))
 pattern Bot :: S.Exp L
 pattern Bot = S.Exp (S.Const (L 0))
 
-pattern L_ :: S.Exp Word8 -> S.Exp L
-pattern L_ x = Pattern x
-{-# COMPLETE L_ #-}
+pattern L_ :: Exp Word8 -> Exp L
+pattern L_ x = Pattern (x)
+
+-- {-# COMPLETE L_ #-}
 
 isTop :: Exp L -> Exp Bool
-isTop Top = S.Exp (S.Const True)
-isTop _ = S.Exp (S.Const False)
+isTop (L_ x) = (x A.== 100)
 
 isBottom :: Exp L -> Exp Bool
-isBottom Bot = S.Exp (S.Const True)
-isBottom _ = S.Exp (S.Const False)
+isBottom (L_ x) = (x A.== 0)
 
 lOne :: Exp L
 lOne = S.Exp (S.Const (L 1))
 
 plus :: Exp L -> Exp L -> Exp L
-plus (L_ x) (L_ y) = L_ (100 `A.min` (x + y))
+plus (L_ x) (L_ y) = L_ (100 `A.min` x + y)
 
 
 plus' :: Exp L -> Exp L -> Exp L
-plus' a b = cond 
-                (isTop a) Top 
-                (cond 
-                    (isTop b) Top 
-                    (cond 
-                        (isBottom a) (cond (isBottom b) Bot Top)
-                        (plus a b)))
+plus' a b = A.cond 
+                    (isTop a) Top (cond (isTop b) Top 
+                    (cond (isBottom a) (cond (isBottom b) Bot Top)
+                    (cond (isBottom b) Top (plus a b))))
 
 lmin :: Exp L -> Exp L -> Exp L
 lmin (L_ x) (L_ y) = L_ (x `A.min` y)
@@ -119,6 +154,7 @@ stepXY = crossStep lmin (plus' lOne)
 stepNPXY :: Acc (A.Array DIM4 L) -> A.Acc (A.Array DIM4 L)
 stepNPXY = liftStencil stepXY  (A.function $ A.const Bot) 
 
+
 sample1x1x3x3 :: Acc (A.Array DIM4 L)
 sample1x1x3x3 = A.generate 
     (A.constant ( Z :. 1 :. 1 :. 3 :. 3))
@@ -126,12 +162,7 @@ sample1x1x3x3 = A.generate
              in z0)
         where 
             startingPoint :: Exp DIM4 -> Exp L
-            startingPoint ix4 = let  (Z:. p :. n :. x :. y) = unlift ix4 :: (Z :. Exp Int :. Exp Int :. Exp Int :. Exp Int)
-                                in start x y
+            startingPoint ( Z_ ::. p ::. n ::. x ::. y) = start x y
             start :: Exp Int -> Exp Int -> Exp L
-            start 1 1 = L_ 1
-            start _ _ = Top
-                
-
--- stepNPXY :: A.Acc (A.Array DIM4 L) -> S.PreSmartAcc A.Acc Identity (A.Array DIM4 L)
--- stepNPXY = liftSmartStencil (S.Constant Bot) stepXY
+            start x y = cond (x A.== y) (cond (x A.== 1) lOne Bot) Top
+            
